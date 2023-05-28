9b701c4411cc5c4f301f38851b20c0f22a859741
drvillota <dr.villota@uniandes.edu.co>
2023-05-27 22:27:06 -0500

    1ras pruebas exploratorias

--- cypress/cypress/e2e/ghost_3.41/gestion_usuarios.spec.cy.js
+++ cypress/cypress/e2e/ghost_3.41/gestion_usuarios.spec.cy.js
@@ -11,11 +11,13 @@ const host = "http://test.denkitronik.com:2368";
 describe("Gestion de usuarios", () => {
   beforeEach(() => {
     // Realizar el inicio de sesión antes de cada escenario
-    cy.visit(host+"/ghost/#/signin");
-    cy.get('input[name="identification"]').clear().type("lasherone@hotmail.com");
+    cy.visit(host + "/ghost/#/signin");
+    cy.get('input[name="identification"]')
+      .clear()
+      .type("lasherone@hotmail.com");
     cy.get('input[name="password"]').clear().type("Pruebas12345");
     //Screenshot de inicio de sesion de usuario admin
-    cy.screenshot("ghost-3.41.1/usuarios/inicio_sesion_admin");  
+    cy.screenshot("ghost-3.41.1/usuarios/inicio_sesion_admin");
     cy.get('button[type="submit"]').click();
   });
 
@@ -24,10 +26,10 @@ describe("Gestion de usuarios", () => {
       // Navegar a la página de gestión de usuarios
       cy.contains("a", "Staff").click();
       //Screenshot de la pagina de staff de usuarios
-      cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/1_staff");  
+      cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/1_staff");
       cy.contains("h3", "Ghost").click();
       //Screenshot de la pagina de usuario ghost
-      cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/2_ghost");  
+      cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/2_ghost");
     });
 
     it("Actualizar información del usuario", () => {
@@ -50,12 +52,14 @@ describe("Gestion de usuarios", () => {
         .focus() // Hacer foco en el elemento
         .clear({ force: true }) // Limpiar el campo forzando la interacción
         .type(newEmail, { force: true }); // Escribir en el campo forzando la interacción
-        //Screenshot de la pagina de usuario ghost editado
-        cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/1_ghost_editado");  
+      //Screenshot de la pagina de usuario ghost editado
+      cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/1_ghost_editado");
       // Guardar los cambios
       cy.contains("span", "Save").parent().click();
       //Screenshot de la pagina de usuario ghost actualizado
-      cy.screenshot("ghost-3.41.1/usuarios/actualizar_usuario/2_ghost_actualizado");  
+      cy.screenshot(
+        "ghost-3.41.1/usuarios/actualizar_usuario/2_ghost_actualizado"
+      );
       // Verificar que la información se haya actualizado correctamente
       cy.get(selectors.slug).scrollIntoView();
       cy.get(selectors.slug).should("have.value", newSlug);
@@ -70,7 +74,7 @@ describe("Gestion de usuarios", () => {
       cy.contains("a", "Staff").click();
       cy.contains("h3", "Ghost").click();
       //Screenshot de la pagina de ghost de cambio de clave
-      cy.screenshot("ghost-3.41.1/usuarios/actualizar_clave/1_ghost");  
+      cy.screenshot("ghost-3.41.1/usuarios/actualizar_clave/1_ghost");
     });
 
     it("Cambiar la contraseña del usuario", () => {
@@ -84,7 +88,7 @@ describe("Gestion de usuarios", () => {
       cy.screenshot("ghost-3.41.1/usuarios/actualizar_clave/2_clave_nueva");
       cy.contains("span", "Change Password").parent().click();
       //Screenshot de la pagina de ghost de cambio de clave con la nueva clave cambiada
-      cy.screenshot("ghost-3.41.1/usuarios/actualizar_clave/3_clave_cambiada");  
+      cy.screenshot("ghost-3.41.1/usuarios/actualizar_clave/3_clave_cambiada");
       // Verificar que la contraseña se haya cambiado correctamente
       cy.contains("span", "Password updated").should("exist");
     });
@@ -94,18 +98,18 @@ describe("Gestion de usuarios", () => {
 describe("Gestion de usuarios de Ghost (suspension y reactivacion)", () => {
   describe("Como usuario admin puedo suspender otro usuario", () => {
     before(() => {
-      cy.visit(host+"/ghost/#/signin");
+      cy.visit(host + "/ghost/#/signin");
       cy.get("#ember8").type("lasherone@hotmail.com");
       cy.get("#ember10").type("Pruebas12345");
       //Screenshot de inicio de sesion para suspender usuario
-      cy.screenshot("ghost-3.41.1/usuarios/suspender/1_inicio_sesion_admin");  
+      cy.screenshot("ghost-3.41.1/usuarios/suspender/1_inicio_sesion_admin");
       cy.get("#ember12").click();
     });
 
     it("Update user", () => {
       cy.contains("Staff").should("be.visible").click();
       //Screenshot de la pagina de staff
-      cy.screenshot("ghost-3.41.1/usuarios/suspender/2_staff"); 
+      cy.screenshot("ghost-3.41.1/usuarios/suspender/2_staff");
       cy.contains("Ghost").should("be.visible").click();
       //Screenshot de la pagina de usuario ghost
       cy.screenshot("ghost-3.41.1/usuarios/suspender/3_ghost");
@@ -118,13 +122,17 @@ describe("Gestion de usuarios de Ghost (suspension y reactivacion)", () => {
       cy.screenshot("ghost-3.41.1/usuarios/suspender/4_ghost_menu_desplegado");
       cy.get("button.suspend").should("be.visible").invoke("click");
       //Screenshot de la pagina de usuario ghost con el menu desplegado y suspendido
-      cy.screenshot("ghost-3.41.1/usuarios/suspender/5_ghost_menu_desplegado_suspendido");
+      cy.screenshot(
+        "ghost-3.41.1/usuarios/suspender/5_ghost_menu_desplegado_suspendido"
+      );
       cy.get("button.gh-btn-red")
         .contains("Suspend")
         .should("be.visible")
         .click();
       //Screenshot de la pagina de usuario ghost con el menu desplegado y suspendido
-      cy.screenshot("ghost-3.41.1/usuarios/suspender/6_ghost_menu_desplegado_suspendido_confirmado");
+      cy.screenshot(
+        "ghost-3.41.1/usuarios/suspender/6_ghost_menu_desplegado_suspendido_confirmado"
+      );
       cy.contains("Staff").should("be.visible").click();
       //Screenshot de la pagina de staff con el usuario suspendido
       cy.screenshot("ghost-3.41.1/usuarios/suspender/7_staff_suspender");
@@ -137,7 +145,7 @@ describe("Gestion de usuarios de Ghost (suspension y reactivacion)", () => {
 
   describe("Como usuario admin puedo reactivar (un-suspend) otro usuario", () => {
     before(() => {
-      cy.visit(host+"/ghost/#/signin");
+      cy.visit(host + "/ghost/#/signin");
       cy.get("#ember8").type("lasherone@hotmail.com");
       cy.get("#ember10").type("Pruebas12345");
       //Screenshot de la pagina de inicio de sesion para reactivar usuario
@@ -155,13 +163,17 @@ describe("Gestion de usuarios de Ghost (suspension y reactivacion)", () => {
       cy.screenshot("ghost-3.41.1/usuarios/unsuspend/3_ghost_menu_desplegado");
       cy.contains("Un-suspend").should("be.visible").click();
       //Screenshot de la pagina de usuario ghost con el menu desplegado y reactivado
-      cy.screenshot("ghost-3.41.1/usuarios/unsuspend/4_ghost_menu_desplegado_reactivado");
+      cy.screenshot(
+        "ghost-3.41.1/usuarios/unsuspend/4_ghost_menu_desplegado_reactivado"
+      );
       cy.get("button.gh-btn-red")
         .contains("Un-suspend")
         .should("be.visible")
         .click();
       //Screenshot de la pagina de usuario ghost con el menu desplegado y reactivado confirmado
-      cy.screenshot("ghost-3.41.1/usuarios/unsuspend/5_ghost_menu_desplegado_reactivado_confirmado");
+      cy.screenshot(
+        "ghost-3.41.1/usuarios/unsuspend/5_ghost_menu_desplegado_reactivado_confirmado"
+      );
       cy.contains("Staff").should("be.visible").click();
       //Screenshot de la pagina de staff reactivado
       cy.screenshot("ghost-3.41.1/usuarios/unsuspend/6_staff_reactivado");
@@ -172,10 +184,11 @@ describe("Gestion de usuarios de Ghost (suspension y reactivacion)", () => {
     });
   });
 });
+
 describe("Gestion de Diseño", () => {
   describe("Como usuario puedo actualizar un item de navegacion", () => {
     beforeEach(() => {
-      cy.visit(host+"/ghost/#/signin");
+      cy.visit(host + "/ghost/#/signin");
       cy.get("#ember8").type("lasherone@hotmail.com");
       cy.get("#ember10").type("Pruebas12345");
       //Screenshot de la pagina de inicio de sesion para actualizar un item de navegacion
@@ -193,11 +206,15 @@ describe("Gestion de Diseño", () => {
         .first()
         .type(randomLabel);
       //Screenshot de la pagina de diseño con el item de navegacion actualizado
-      cy.screenshot("ghost-3.41.1/diseño/actualizar/3_diseño_item_navegacion_actualizado");
+      cy.screenshot(
+        "ghost-3.41.1/diseño/actualizar/3_diseño_item_navegacion_actualizado"
+      );
       cy.contains("button", "Save").click();
       cy.contains("Design").click();
       //Screenshot de la pagina de diseño con el item de navegacion actualizado y guardado
-      cy.screenshot("ghost-3.41.1/diseño/actualizar/4_diseño_item_navegacion_actualizado_guardado");
+      cy.screenshot(
+        "ghost-3.41.1/diseño/actualizar/4_diseño_item_navegacion_actualizado_guardado"
+      );
       cy.get("input")
         .filter((index, el) => el.value === randomLabel)
         .first()
